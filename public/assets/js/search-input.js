58299840a01914534d5b36e283a6cdc248414afc
Kevin CHALLIT <kevin.challit@gmail.fr>
2021-10-16 21:47:04 +0200

    update documentation > doc technique

--- public/assets/js/search-input.js
+++ public/assets/js/search-input.js
@@ -2,11 +2,12 @@ const input = document.getElementById('search-input');
 const dropdown = document.getElementById('dropdown-search');
 const containerDropdown = document.querySelector('.dropdown-search-container');
 
-input.addEventListener('keydown', async (e) => {
+input.addEventListener('keydown', async () => {
     let searchWord = input.value;
 
-    if(input.value.length > 1) {
-        let response = await fetch('http://localhost:8000/api/books');
+console.log(searchWord.length)
+    if(searchWord.length >= 1){
+        let response = await fetch('http://127.0.0.1:8000/api/books');
         if(response.ok && response.status === 200) {
             let data = await response.json();
             const searchResult = await data.filter(book => book.title.includes(searchWord))
@@ -20,12 +21,12 @@ const displayResults = (results) => {
     containerDropdown.style.display = 'inline-block';
     if(results.length === 0) {
         const li = document.createElement('li');
-        li.textContent = 'Aucun livre ne correspond à votre recherche'
+        li.textContent = 'Aucun livre ne correspond à votre recherche';
         dropdown.append(li)
     } else {
         for(let i = 0; i < results.length; i++) {
             const li = document.createElement('li');
-            li.insertAdjacentHTML('afterbegin', `<a href="http://localhost:8000/books/${results[i].id}"> ${results[i].title} </a>` )
+            li.insertAdjacentHTML('afterbegin', `<a href="http://127.0.0.1:8000/books/${results[i].id}"> ${results[i].title} </a>` )
             dropdown.append(li)
         }
     }
--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -12,9 +12,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class RegistrationController extends AbstractController
 {
-    /**
-     * @Route("/register", name="app_register")
-     */
+    #[Route('/register', name: 'app_register')]
     public function register(Request $request, UserPasswordHasherInterface $userPasswordHasherInterface): Response
     {
         $user = new Users();
