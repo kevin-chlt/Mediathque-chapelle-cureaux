14afde4aaf88df0eddc2c1104a915136c981c57a
Kevin CHALLIT <kevin.challit@gmail.fr>
2021-10-31 22:29:47 +0100

    Change htaccess rules

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -7,6 +7,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\BirthdayType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\FormEvent;
@@ -101,7 +102,7 @@ class RegistrationFormType extends AbstractType
                     ])
                 ]
             ])
-            ->add('address', TextType::class, [
+            ->add('address', TextareaType::class, [
                 'label' => 'Adresse',
                 'constraints' => [
                     new NotBlank([
