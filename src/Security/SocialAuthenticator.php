4053cf1d048c4ecd82a0737dd7a4fc65a99e2f0e
Kevin CHALLIT <kevin.challit@gmail.fr>
2021-11-04 21:20:13 +0100

    add OAuth with google
    add https secure
    
    remov https
    
    change htaccess
    
    rewrite request_uri
    
    change htaccess
    
    rewrite htaccess
    
    add htaccess
    
    update htaccess
    
    update ssl
    
    update htaccess
    
    add htaccess
    
    add htaccess
    
    update htaccess
    
    add
    
    add https schemes
    
    change htaccess
    
    change htaccess
    
    add  proto
    
    remove  dd
    
    add request scheme
    
    add require channel
    
    add dd
    
    fix https bug
    
    update gAuth
    
    add https
    
    add trusted-proxys
    
    add https
    
    add error login oauth

--- src/Security/SocialAuthenticator.php
+++ src/Security/SocialAuthenticator.php
@@ -12,6 +12,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\RouterInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\PassportInterface;
 use Symfony\Component\Security\Http\Authenticator\Passport\SelfValidatingPassport;
@@ -21,12 +22,14 @@ class SocialAuthenticator extends OAuth2Authenticator
     private $clientRegistry;
     private $userRepository;
     private $router;
+    private $authenticationUtils;
 
-    public function __construct(ClientRegistry $clientRegistry, UsersRepository $usersRepository, RouterInterface $router)
+    public function __construct(ClientRegistry $clientRegistry, UsersRepository $usersRepository, RouterInterface $router, AuthenticationUtils $authenticationUtils)
     {
         $this->clientRegistry = $clientRegistry;
         $this->userRepository = $usersRepository;
         $this->router = $router;
+        $this->authenticationUtils = $authenticationUtils;
     }
 
     public function supports(Request $request): ?bool
@@ -44,7 +47,6 @@ class SocialAuthenticator extends OAuth2Authenticator
             new UserBadge($accessToken->getToken(), function () use ($accessToken, $client) {
                 /** @var GoogleUser $googleUser */
                 $googleUser = $client->fetchUserFromToken($accessToken);
-
                 $email = $googleUser->getEmail();
 
                 return $this->userRepository->findOneBy(['email' => $email]);
@@ -61,8 +63,12 @@ class SocialAuthenticator extends OAuth2Authenticator
 
     public function onAuthenticationFailure(Request $request, AuthenticationException $exception): ?Response
     {
+        // get the login error if there is one
+        $error = $this->authenticationUtils->getLastAuthenticationError();
+
+
         $message = strtr($exception->getMessageKey(), $exception->getMessageData());
-        $request->getSession()->getFlashBag()->add('errors', $message);
+        $request->getSession()->getFlashBag()->add('errors',  $error);
 
         return new RedirectResponse($this->router->generate('app_login'));
     }
