7514c58427348032481b92919f825567f24d93cd
Kevin CHALLIT <kevin.challit@gmail.fr>
2021-11-08 20:46:58 +0100

    add phpunit

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -13,12 +13,8 @@ use Symfony\Component\Form\FormEvent;
 use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Security\Core\Validator\Constraints\UserPassword;
-use Symfony\Component\Validator\Constraints\GreaterThan;
 use Symfony\Component\Validator\Constraints\Length;
-use Symfony\Component\Validator\Constraints\LessThan;
-use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
-use Symfony\Component\Validator\Constraints\Type;
 
 class RegistrationFormType extends AbstractType
 {
@@ -28,17 +24,7 @@ class RegistrationFormType extends AbstractType
             ->add('email', EmailType::class, [
                 'label' => 'Adresse e-mail',
                 'disabled' => $options['is_edit'],
-                'constraints' => [
-                    new notBlank([
-                        'message' => 'Vous devez remplir ce champ.',
-                    ]),
-                    new Length([
-                        'min' => 4,
-                        'minMessage' => 'Votre email doit contenir au moins {{ limit }} caractères.',
-                        'max' => 255,
-                        'maxMessage' => 'Votre email doit contenir au moins {{ limit }} caractères.'
-                    ]),
-                ],
+                'invalid_message' => 'L\'email doit être au format abc@abc.ab'
             ])
             ->add('plainPassword', RepeatedType::class, [
                 'type' => PasswordType::class,
@@ -66,72 +52,18 @@ class RegistrationFormType extends AbstractType
         ->add('lastname', TextType::class, [
             'label' => 'Prénom',
             'disabled' => $options['is_edit'],
-            'constraints' => [
-                new NotBlank([
-                    'message' => 'Vous devez remplir ce champ.'
-                ]),
-                new Length([
-                    'min' => 2,
-                    'minMessage' => 'Votre prénom doit contenir au moins {{ limit }} caractères.',
-                    'max' => 255,
-                    'maxMessage' => 'Votre prénom doit contenir au maximum {{ limit }} caractères.',
-                ]),
-                new Type([
-                    'type' => 'alpha',
-                    'message' => 'Votre prénom doit être composé uniquement de lettres et sans caractères spécial.',
-                ])
-            ]
         ])
             ->add('firstname', TextType::class, [
                 'label' => 'Nom',
                 'disabled' => $options['is_edit'],
-                'constraints' => [
-                    new NotBlank([
-                        'message' => 'Vous devez remplir ce champ.'
-                    ]),
-                    new Length([
-                        'min' => 2,
-                        'minMessage' => 'Votre nom doit contenir au moins {{ limit }} caractères.',
-                        'max' => 255,
-                        'maxMessage' => 'Votre nom doit contenir au maximum {{ limit }} caractères.',
-                    ]),
-                    new Type([
-                        'type' => 'alpha',
-                        'message' => 'Votre nom doit être composé uniquement de lettres et sans caractères spécial.',
-                    ])
-                ]
             ])
             ->add('address', TextType::class, [
                 'label' => 'Adresse',
-                'constraints' => [
-                    new NotBlank([
-                        'message' => 'Vous devez remplir ce champ.'
-                    ]),
-                    new Length([
-                        'min' => 2,
-                        'minMessage' => 'Votre adresse doit contenir au moins {{ limit }} caractères.',
-                        'max' => 255,
-                        'maxMessage' => 'Votre adresse doit contenir au maximum {{ limit }} caractères.',
-                    ])
-                ]
             ])
             ->add('birthdate', BirthdayType::class, [
                 'disabled' => $options['is_edit'],
                 'label' => 'Date de naissance',
                 'widget'=> 'single_text',
-                'constraints' => [
-                    new NotBlank([
-                        'message' => 'Vous devez remplir ce champ.'
-                    ]),
-                    new GreaterThan([
-                        'value' => '-120 years',
-                        'message' => 'Date de naissance incorrect.'
-                    ]),
-                    new LessThan([
-                        'value' => '-6 years',
-                        'message' => 'Vous devez avoir 6 ans minimum pour adhérer à la médiathèque.'
-                    ])
-                ]
             ])
         ->addEventListener(FormEvents::PRE_SET_DATA, function (FormEvent $events){
             $form = $events->getForm();
---
+++ tests/Entity/UsersTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Tests\Entity;
+
+use App\Entity\Users;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+class UsersTest extends KernelTestCase
+{
+    public function getEntity () : Users {
+        return (new Users())
+            ->setFirstname('John')
+            ->setLastname('Doe')
+            ->setEmail('john@doe.fr')
+            ->setPassword('password');
+    }
+
+    public function assertHasError (Users $user, int $errorExpected = 0) {
+        self::bootKernel();
+        $error = static::getContainer()->get('validator')->validate($user);
+        $this->assertCount($errorExpected, $error);
+    }
+
+    public function testValidEntity () {
+        $this->assertHasError($this->getEntity());
+        $this->assertHasError($this->getEntity()->setFirstname('Kévîn'));
+        $this->assertHasError($this->getEntity()->setFirstname('Jean-Christophe'));
+    }
+
+    public function testInvalidFirstname () {
+       $this->assertHasError($this->getEntity()->setFirstname(''), 1);
+       $this->assertHasError($this->getEntity()->setFirstname('Jo4nn'), 1);
+       $this->assertHasError($this->getEntity()->setFirstname('4534'), 1);
+       $this->assertHasError($this->getEntity()->setFirstname('33'), 2);
+       $this->assertHasError($this->getEntity()->setFirstname('ju'), 1);
+       $this->assertHasError($this->getEntity()->setFirstname('pseudo avec espace'), 1);
+    }
+}
